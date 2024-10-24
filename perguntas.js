cd41a85d40b29441ad81bc356d303ee42ec79979
ketllymunithelly <140863030+ketllymunithelly@users.noreply.github.com>
2024-10-23 22:29:23 -0300

    k

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Felicidades',
-    'O que precisa para ser feliz?',
-    ' Fazer o que gosta!!!'
+  'Felicidades',
+    'qual sua comida favorita?',
+    'churrasco'
 )
 
 criaCartao(
     'Informática',
-    'Qual o melhor canal para aprender informática',
-    '@rasinformática'
+    'qual seu hobby preferido?',
+    'computação'
 )
 
 criaCartao(
-    'Qual é a melhor turma do CERB',
-    'A que mais se dedica as aulas do Prof. Rafael?',
-    'Estou em dúvida'
+    'Música',
+    'qual o melhor estilo musical?',
+    'sertanejo'
 )
 
 criaCartao(
-    'Ganhando o Mundo',
-    'Quem participou do Programa Ganhado o mundo em 2024?',
-    'Eloisa Amorim dos Santos 👏👏👏'
+    'Drink',
+    'Qual é minha bebida favorita?',
+    ' coca-cola👏👏👏'
 )
