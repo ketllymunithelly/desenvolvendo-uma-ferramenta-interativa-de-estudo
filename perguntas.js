08c380336a3f7cde123e5bccd25a4316a5c60a2b
ketllymunithelly <140863030+ketllymunithelly@users.noreply.github.com>
2024-10-23 22:19:54 -0300

    g

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Felicidades',
+    'qual sua comida favorita?',
+    'churrasco'
+)
+
+criaCartao(
+    'Informática',
+    'qual seu hobby preferido?',
+    'computação'
+)
+
+criaCartao(
+    'Música',
+    'qual o melhor estilo musical?',
+    'sertanejo'
+)
+
+criaCartao(
+    'Drink',
+    'Qual é minha bebida favorita?',
+    ' coca-cola👏👏👏'
+)
